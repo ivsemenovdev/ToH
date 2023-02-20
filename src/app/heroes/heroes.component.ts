f915648d5e09f4825bb8b4b6811481d90debecbd
Ivan Semenov <ivsemenovdev@gmail.com>
2023-02-20 17:45:51 +0300

    feat: change receiving data in hero service on observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero";
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {
+  };
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
     // console.log(hero)
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
