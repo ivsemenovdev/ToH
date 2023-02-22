48f077cb371c115adb1e0188ec1ba2fa4cf2bf70
Ivan Semenov <ivsemenovdev@gmail.com>
2023-02-22 11:38:01 +0300

    feat: add update heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,6 +13,10 @@ import { catchError, map, tap } from 'rxjs/operators';
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService,
@@ -62,5 +66,13 @@ export class HeroService {
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 }
