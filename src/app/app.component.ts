471215070017de010bc1e57d51d8f101ad8db746
Ivan Semenov <semenov.i@toredo.ru>
2023-09-29 18:00:15 +0300

    add data service logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {DataService} from "./services/data.service";
-import {map, tap} from "rxjs";
+import {map, Subject, tap} from "rxjs";
 import {ConvertService} from "./services/convert.service";
 import {logMessages} from "@angular-devkit/build-angular/src/tools/esbuild/utils";
 
@@ -10,43 +10,59 @@ import {logMessages} from "@angular-devkit/build-angular/src/tools/esbuild/utils
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  observable$ = this.dataService.get();
+
   title = 'Learning';
-  obj: {[index: string]:string} = {}
-  char: string = ''
-  res: Array<Object> = [];
+  obj: { [index: string]: string } = {}
+  res: Array<{[index: string]: string}> = [];
+  isClicked = false
 
   constructor(private dataService: DataService, private convertService: ConvertService) {
   }
 
   ngOnInit() {
-    this.dataService.get()
+    this.observable$
       .pipe(
-        // tap(_ => console.log(_))
-      )
-      .subscribe(
-        el => {
-          this.char = this.convertService.getRandomChar();
-          this.obj[el] = this.char
-          this.res.push(this.obj)
-        });
+        tap(_ => console.log("stream el", _)),
+        map(value => {
+          this.obj[value] = this.convertService.getRandomChar()
+          return this.obj
+        }))
+      .subscribe(val => {
+        this.res.push(this.obj);
+        this.obj = {}
+        console.log("piped el", val)
+      })
     console.log(this.res)
   }
 
-  // addElement() {
-  //   let r = (Math.floor(Math.random() * 10)).toString();
-  //   console.log("random", r);
-  //   console.log('Add')
-  //   this.dataService.getRandomChar();
-  //   // this.dataService.set(1)
-  //   this.dataService.get()
-  //     .subscribe({
-  //         //   res => {
-  //         // this.array = res;
-  //         // console.log(this.array);
-  //         next: value => console.log('next:', value),
-  //         error: err => console.log('error:', err),
-  //         complete: () => console.log('the end'),
-  //       }
-  //     );
-  // }
+  addElement() {
+    this.dataService.set();
+    this.res = [];
+    this.observable$
+      .pipe(
+        tap(_ => console.log("stream el", _)),
+        map(value => {
+          this.obj[value] = this.convertService.getRandomChar()
+          return this.obj
+        }))
+      .subscribe(val => {
+        this.res.push(this.obj);
+        this.obj = {}
+        console.log("piped el", val)
+      })
+    console.log(this.res)
+  }
+
+  returnZero() {
+    return 0
+  }
+
+  // log(event) {
+  clickHandler(el: any, key: any) {
+    console.log(el.value[key])
+  }
+
+  protected readonly Object = Object;
 }
